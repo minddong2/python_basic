f3be97f07b3ec88b640369311d644892f9378fb7
KMJ <qnsghdharo119@naver.com>
2023-11-08 20:41:25 +0900

    1920_2

---
+++ 1920_2.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+A = set(map(int,input().split()))
+M = int(input())
+B = list(map(int,input().split()))
+
+for i in B:
+    print(1) if i in A else print(0)
